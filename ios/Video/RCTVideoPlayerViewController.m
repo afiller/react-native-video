2829cb4b27b62725105cc5ec194fbc13c0e48f7e
Andreas Filler <andreas@filler.name>
2019-05-30 13:22:21 +0200

    fix for NSInternalInconsistencyException

--- ios/Video/RCTVideoPlayerViewController.m
+++ ios/Video/RCTVideoPlayerViewController.m
@@ -10,50 +10,54 @@
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        Class class = [self class];
-        
-        SEL originalSelector = @selector(addObserver:forKeyPath:options:context:);
-        SEL swizzledSelector = @selector(swizzled_addObserver:forKeyPath:options:context:);
-        
-        Method originalMethod = class_getInstanceMethod(class, originalSelector);
-        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-        
-        SEL originalSelectorDealloc = @selector(dealloc:);
-        SEL swizzledSelectorDealloc = @selector(swizzled_dealloc:);
-        
-        Class classDealloc = object_getClass((id)self);
-        
-        Method originalMethodDealloc = class_getClassMethod(classDealloc, originalSelectorDealloc);
-        Method swizzledMethodDealloc = class_getClassMethod(classDealloc, swizzledSelectorDealloc);
-        
-        BOOL didAddMethod =
-        class_addMethod(class,
-                        originalSelector,
-                        method_getImplementation(swizzledMethod),
-                        method_getTypeEncoding(swizzledMethod));
-        
-        if (didAddMethod) {
-            class_replaceMethod(class,
-                                swizzledSelector,
-                                method_getImplementation(originalMethod),
-                                method_getTypeEncoding(originalMethod));
+        if (@available(iOS 11, *)) {
+            // Don't swizzle on iOS 11+
         } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
-        
-        BOOL didAddMethodDealloc =
-        class_addMethod(classDealloc,
-                        originalSelectorDealloc,
-                        method_getImplementation(swizzledMethodDealloc),
-                        method_getTypeEncoding(swizzledMethodDealloc));
-        
-        if (didAddMethodDealloc) {
-            class_replaceMethod(classDealloc,
-                                swizzledSelectorDealloc,
-                                method_getImplementation(originalMethodDealloc),
-                                method_getTypeEncoding(originalMethodDealloc));
-        } else {
-            method_exchangeImplementations(originalMethodDealloc, swizzledMethodDealloc);
+            Class class = [self class];
+            
+            SEL originalSelector = @selector(addObserver:forKeyPath:options:context:);
+            SEL swizzledSelector = @selector(swizzled_addObserver:forKeyPath:options:context:);
+            
+            Method originalMethod = class_getInstanceMethod(class, originalSelector);
+            Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+            
+            SEL originalSelectorDealloc = @selector(dealloc:);
+            SEL swizzledSelectorDealloc = @selector(swizzled_dealloc:);
+            
+            Class classDealloc = object_getClass((id)self);
+            
+            Method originalMethodDealloc = class_getClassMethod(classDealloc, originalSelectorDealloc);
+            Method swizzledMethodDealloc = class_getClassMethod(classDealloc, swizzledSelectorDealloc);
+            
+            BOOL didAddMethod =
+            class_addMethod(class,
+                            originalSelector,
+                            method_getImplementation(swizzledMethod),
+                            method_getTypeEncoding(swizzledMethod));
+            
+            if (didAddMethod) {
+                class_replaceMethod(class,
+                                    swizzledSelector,
+                                    method_getImplementation(originalMethod),
+                                    method_getTypeEncoding(originalMethod));
+            } else {
+                method_exchangeImplementations(originalMethod, swizzledMethod);
+            }
+            
+            BOOL didAddMethodDealloc =
+            class_addMethod(classDealloc,
+                            originalSelectorDealloc,
+                            method_getImplementation(swizzledMethodDealloc),
+                            method_getTypeEncoding(swizzledMethodDealloc));
+            
+            if (didAddMethodDealloc) {
+                class_replaceMethod(classDealloc,
+                                    swizzledSelectorDealloc,
+                                    method_getImplementation(originalMethodDealloc),
+                                    method_getTypeEncoding(originalMethodDealloc));
+            } else {
+                method_exchangeImplementations(originalMethodDealloc, swizzledMethodDealloc);
+            }
         }
     });
 }
